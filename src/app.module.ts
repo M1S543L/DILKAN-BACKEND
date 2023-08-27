1d053c3266e39d72e5f17dd09ad063cbec8d6bff
M1S543L <dilan.almendarez@unah.hn>
2023-08-26 18:29:52 -0600

    Pa cargarlo a produccion

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { AppService } from './app.service';
 import { UsuarioModule } from './usuario/usuario.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProyectoModule } from './proyecto/proyecto.module';
-
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [ UsuarioModule, MongooseModule.forRoot('mongodb+srv://M1S543L:DilanAndino1994@clusterdilkan.pll3f9l.mongodb.net/'),
+  imports: [ ConfigModule.forRoot(), UsuarioModule, MongooseModule.forRoot(
+    'mongodb+srv://M1S543L:DilanAndino1994@clusterdilkan.pll3f9l.mongodb.net/'),
   ProyectoModule],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors({
-    origin: 'http://localhost:4200', // Cambia esto a la URL de tu frontend Angular
+    origin: 'https://dilkan-de073.web.app/login', // Cambia esto a la URL de tu frontend Angular
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   }));
  
-  await app.listen(3000);
+  await app.listen(process.env.PORT ||3000);
 }
 bootstrap();
