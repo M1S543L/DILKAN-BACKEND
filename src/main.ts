f6ad5b4530089c8738f0dc0818932672de2cc9d8
M1S543L <dilan.almendarez@unah.hn>
2023-08-26 19:17:06 -0600

    Para produccion 2

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors({
-    origin: 'https://dilkan-de073.web.app/login', // Cambia esto a la URL de tu frontend Angular
+    origin: 'https://dilkan-de073.web.app', // Cambia esto a la URL de tu frontend Angular
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   }));
